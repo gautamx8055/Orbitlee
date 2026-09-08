72793f710ceaccf8ff90fda5e33df64b558d5b1e
Gautam <gautam@Gautams-MacBook-Air.local>
2026-09-08 23:48:29 +0530

    sitemap changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,28 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
+
+const skip = [
+	'/lab/',
+	'/404',
+	'/services/marketing-advertising',
+	'/services/web-app-development',
+	'/sitemap',
+];
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.orbitlee.com',
-  trailingSlash: 'ignore',
-  adapter: vercel(),
+	site: 'https://www.orbitlee.com',
+	trailingSlash: 'ignore',
+	adapter: vercel(),
+	integrations: [
+		sitemap({
+			filter: (page) => !skip.some((path) => page.includes(path)),
+			serialize(item) {
+				const url = item.url.endsWith('/') || item.url.match(/\.[a-z0-9]+$/i) ? item.url : `${item.url}/`;
+				return { ...item, url };
+			},
+		}),
+	],
 });
